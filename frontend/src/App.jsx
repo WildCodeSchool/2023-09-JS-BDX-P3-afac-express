63b30db3db002fc1fb007e3bad252164ad5d51ad
Benoit-Hayet <hayetbenoit@gmail.com>
2023-12-11 12:36:19 +0100

    Add Navlink

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
+import Navigation from "./components/Navigation";
 
 function App() {
   return (
     <div>
       <Outlet />
+      <Navigation />
     </div>
   );
 }
