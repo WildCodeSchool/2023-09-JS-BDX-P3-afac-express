c15e639d37ab746d6ee3b6f4b34fa35613bbc220
Florian BRUN <flo33.brun@hotmail.fr>
2023-12-11 23:49:02 +0100

    Added style on Footer.jsx component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,11 @@
-import { Outlet } from "react-router-dom";
 import "./App.css";
 import Footer from "./components/Footer";
+import "./pages/Style/Footer.scss";
 
 function App() {
   return (
     <div>
       <Footer />
-      <Outlet />
     </div>
   );
 }
--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,26 +3,33 @@ function Footer() {
     <div className="container-footer">
       <div className="contact">
         <h2 className="title-contact">Nous contacter</h2>
-        <img src="./src/assets/footer/logoTelephone.svg" alt="phone-logo" />
-        <p>+262 692 31 83 98</p>
-        <img src="./src/assets/footer/logoMail.svg" alt="mail-logo" />
-        <div className="mail-adress">
-          <p>afac974@gmail.com</p>
+        <div className="container flex">
+          <img src="./src/assets/footer/logoTelephone.svg" alt="phone-logo" />
+          <p className="telephone-number">+262 692 31 83 98</p>
+        </div>
+        <div className="container flex">
+          <img src="./src/assets/footer/logoMail.svg" alt="mail-logo" />
+          <p className="adress-mail">afac974@gmail.com</p>
         </div>
         <div className="general-information">
           <p>Mentions Générales</p>
         </div>
       </div>
+      <span className="vertical-line"> </span>
       <div className="partnerships">
         <h2 className="title-partnerships">Partenaires</h2>
-        <img
-          src="./src/assets/footer/logoObjetTemoin.svg"
-          alt="objet-temoin-logo"
-        />
-        <img
-          src="./src/assets/footer/logoDepartementReunion.svg"
-          alt="reunion-department-logo"
-        />
+        <div className="container flex">
+          <img
+            className="logo-left"
+            src="./src/assets/footer/logoObjetTemoin.svg"
+            alt="objet-temoin-logo"
+          />
+          <img
+            className="logo-right"
+            src="./src/assets/footer/logoDepartementReunion.svg"
+            alt="reunion-department-logo"
+          />
+        </div>
         <div className="copyright">
           <p>©2023-2024 Afac express</p>
         </div>
