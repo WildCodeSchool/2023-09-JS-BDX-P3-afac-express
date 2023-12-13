436b25cc05b9bdf53f1767a397ad844361f84422
Nelia <nelia.courtais@live.fr>
2023-12-13 11:51:04 +0100

    work in progress

---
+++ frontend/src/context/AppContext.jsx
@@ -0,0 +1,10 @@
+// import { createContext, useContext } from "react";
+
+// const appContext = createContext();
+
+// function AppContextProvider({ children }) {
+//   return <appContext.provider>{children}</appContext.provider>;
+// }
+
+// export default AppContextProvider;
+// export const useApp = () => useContext(appContext);
