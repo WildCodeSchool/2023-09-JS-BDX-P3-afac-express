12862ca01803aa507402946f22b6732c9414642d
Nelia <nelia.courtais@live.fr>
2023-12-13 13:53:29 +0100

    Mobile changes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,8 +4,8 @@ import "../App.css";
 import ryandavechetty from "../assets/artists/1_RyanDaveChetty.jpg";
 import deepabauhadoor from "../assets/artists/2_DeepaBauhadoor.jpg";
 import mahefadimbiniainadandrianarivelo from "../assets/artists/3_MahefaDimbiniainaRandrianarivelo.jpg";
-// import mathildeneri from "../assets/artists/4_MathildeNeri.jpg";
-// import larasousa from "../assets/artists/5_LaraSousa.jpg";
+import mathildeneri from "../assets/artists/4_MathildeNeri.jpg";
+import larasousa from "../assets/artists/5_LaraSousa.jpg";
 import creolization from "../assets/artworks/1_Creolization.jfif";
 import bredechouchou from "../assets/artworks/4_BredeChouchou.jfif";
 import analakelyantananarivo from "../assets/artworks/9_AnalakelyAntananarivo.jfif";
@@ -32,92 +32,90 @@ function Home() {
         <h2>Artistes</h2>
 
         <div className="container-artist">
-          <div className="title-img-artist">
-            <div className="artist">
-              <h3>Ryan Dave Chetty</h3>
+          <div className="first-container-artist">
+            <div className="title-img-artist">
+              <div className="artist">
+                <h3>Ryan Dave Chetty</h3>
+              </div>
+              <img src={ryandavechetty} alt="Ryan Dave Chetty" />
             </div>
-            <img src={ryandavechetty} alt="Ryan Dave Chetty" />
+
+            <div className="title-img-artist">
+              <div className="artist">
+                <h3>Deepa Bauhadoor</h3>
+              </div>
+              <img src={deepabauhadoor} alt="Deepa Bauhadoor" />
+            </div>
+          </div>
+          <div className="second-container-artist">
+            <div className="title-img-artist">
+              <div className="artist">
+                <h3>Mahefa D. Randrianarivelo</h3>
+              </div>
+              <img
+                src={mahefadimbiniainadandrianarivelo}
+                alt="Mahefa D. Randrianarivelo"
+              />
+            </div>
+
+            <div className="title-img-artist">
+              <div className="artist">
+                <h3>Mathilde Neri</h3>
+              </div>
+              <img src={mathildeneri} alt="Mathilde Neri" />
+            </div>
           </div>
 
-          <div className="title-img-artist">
-            <div className="artist">
-              <h3>Deepa Bauhadoor</h3>
-            </div>
-            <img src={deepabauhadoor} alt="Deepa Bauhadoor" />
+          <div className="third-container-artist">
+            <div className="title-img-artist">
+              <div className="artist">
+                <h3>Lara Sousa</h3>
+              </div>
+              <img src={larasousa} alt="Lara Sousa" />
+            </div>{" "}
           </div>
-
-          <div className="title-img-artist">
-            <div className="artist">
-              <h3>Mahefa D. Randrianarivelo</h3>
-            </div>
-            <img
-              src={mahefadimbiniainadandrianarivelo}
-              alt="Mahefa D. Randrianarivelo"
-            />
-          </div>
-
-          {/*           
-          <div className="title-img-artist">
-            <div className="artist">
-          //     <h3>Mathilde Neri</h3>
-          //     
-          //   </div>
-          //   <img src={mathildeneri} alt="Mathilde Neri" />
-          // </div> 
-
-          // 
-          <div className="title-img-artist">
-            <div className="artist">
-          //     <h3>Lara Sousa</h3>
-          //     
-          //   </div>
-          //   <img src={larasousa} alt="Lara Sousa" />
-          // </div> */}
         </div>
-        <div className="container-collection-title">
-          <h2>Collection</h2>
-          <p>Regarder le monde c'est toucher sa diversité.</p>
+      </div>
+      <div className="container-collection-title">
+        <h2>Collection</h2>
+        <p>Regarder le monde c'est toucher sa diversité.</p>
 
-          <ul className="horizontal-list">
-            <div className="first-container_collection">
-              <li className="collection">
-                <img src={creolization} alt="Creolization" />
-                <div className="collection-text">
-                  <h3>Creolization</h3>
-                  <h4>Ryan Dave Chetty</h4>
-                </div>
-              </li>
+        <ul className="horizontal-list">
+          <div className="first-container_collection">
+            <li className="collection">
+              <img src={creolization} alt="Creolization" />
+              <div className="collection-text">
+                <h3>Creolization</h3>
+                <h4>Ryan Dave Chetty</h4>
+              </div>
+            </li>
 
-              <li className="collection">
-                <img src={bredechouchou} alt="Deepa Bauhadoor" />
-                <div className="collection-text">
-                  <h3>Brède chouchou</h3>
-                  <h4>Deepa Bauhadoor</h4>
-                </div>
-              </li>
-            </div>
-            <div className="second-container_collection">
-              <li className="collection">
-                <img
-                  src={analakelyantananarivo}
-                  alt="Analakely, Antananarivo"
-                />
-                <div className="collection-text">
-                  <h3>Analakely, Antananarivo</h3>
-                  <h4>Mahefa Dimbiniaina Randrianarivelo</h4>
-                </div>
-              </li>
+            <li className="collection">
+              <img src={bredechouchou} alt="Deepa Bauhadoor" />
+              <div className="collection-text">
+                <h3>Brède chouchou</h3>
+                <h4>Deepa Bauhadoor</h4>
+              </div>
+            </li>
+          </div>
+          <div className="second-container_collection">
+            <li className="collection">
+              <img src={analakelyantananarivo} alt="Analakely, Antananarivo" />
+              <div className="collection-text">
+                <h3>Analakely, Antananarivo</h3>
+                <h4>Mahefa Dimbiniaina Randrianarivelo</h4>
+              </div>
+            </li>
 
-              <li className="collection">
-                <img src={ilneresteplusque} alt="Il ne reste plus que [...]" />
-                <div className="collection-text">
-                  <h3>Il ne reste plus que [...]</h3>
-                  <h4>Mathilde Neri</h4>
-                </div>
-              </li>
-            </div>
-          </ul>
-        </div>
+            <li className="collection">
+              <img src={ilneresteplusque} alt="Il ne reste plus que [...]" />
+              <div className="collection-text">
+                <h3>Il ne reste plus que [...]</h3>
+                <h4>Mathilde Neri</h4>
+              </div>
+            </li>
+          </div>
+        </ul>
       </div>
     </div>
   );
