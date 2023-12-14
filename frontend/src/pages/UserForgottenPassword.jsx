eaaf015742049fd9fb05fe630d2eb5b67ce6063b
Benoit-Hayet <hayetbenoit@gmail.com>
2023-12-14 15:30:29 +0100

    User Pasword

--- frontend/src/pages/UserForgottenPassword.jsx
+++ frontend/src/pages/UserForgottenPassword.jsx
@@ -1,11 +1,20 @@
 import React from "react";
+import { MDBInput, MDBBtn } from "mdb-react-ui-kit";
 
-function UserForgottenPassword() {
+export default function UserForgottenPassword() {
   return (
-    <div>
-      <h2>Hello</h2>
-    </div>
+    <form className="user-form">
+      <MDBInput
+        className="mb-4"
+        type="email"
+        id="form1Example1"
+        label="Email address"
+      />
+      {/* <MDBRow className="mb-4"></MDBRow> */}
+
+      <MDBBtn type="submit" block>
+        Sign in
+      </MDBBtn>
+    </form>
   );
 }
-
-export default UserForgottenPassword;
