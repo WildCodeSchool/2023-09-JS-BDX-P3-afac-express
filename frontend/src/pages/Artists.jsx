795735cc942ac51b1a7209ec5823202e45ce4351
Nelia <nelia.courtais@live.fr>
2023-12-14 15:04:46 +0100

    add style about page

--- frontend/src/pages/Artists.jsx
+++ frontend/src/pages/Artists.jsx
@@ -10,32 +10,7 @@ import mahefadimbiniainadandrianarivelo from "../assets/artists/3_MahefaDimbinia
 
 function Artists() {
   return (
-    <>
-      {/* <div className="container-artist">
-        <div className="all-artist">
-          <h2>Mahefa Dimbiniaina Randrianarivelo</h2>
-          <h3>Photographe</h3>
-          <img
-            src={mahefadimbiniainadandrianarivelo}
-            className="img-fluid"
-            alt="Mahefa Dimbiniaina Randrianarivelo"
-          />
-          <div className="artist-description">
-            <p>
-              Mahefa Dimbiniaina Randrianarivelo, né à Antananarivo en 1991, est
-              un photographe surréaliste Malagasy.Commençant par le graphic
-              design et le digital painting, il choisit d'approfondir la
-              photographie pour s'exprimer. Inspiré par les grands noms du
-              surréalisme : René Magritte, Claude Cahun, Man Ray, plus
-              récemment, Erik Johansson ou Grégory Crewdson, sa principale
-              source d'inspiration reste le cinéaste Wes Anderson.En 2022, il
-              est le lauréat du prix Paritana, prix d'art contemporain malagasy
-              et effectue ainsi une résidence de création d'une durée de trois
-              mois à la Cité Internationale des Arts à Paris.
-            </p>
-          </div>
-        </div>
-      </div> */}
+    <div>
       <MDBContainer fluid>
         <h2 className="fs-2 text text-center fw-bold pt-5">
           Mahefa Dimbiniaina Randrianarivelo
@@ -57,7 +32,7 @@ function Artists() {
             />
           </MDBRipple>
           <MDBCardBody>
-            <MDBCardText className="p-5">
+            <MDBCardText className="p-5 text-start">
               Mahefa Dimbiniaina Randrianarivelo, né à Antananarivo en 1991, est
               un photographe surréaliste Malagasy.Commençant par le graphic
               design et le digital painting, il choisit d'approfondir la
@@ -72,7 +47,7 @@ function Artists() {
           </MDBCardBody>
         </MDBCard>
       </MDBContainer>
-    </>
+    </div>
   );
 }
 
