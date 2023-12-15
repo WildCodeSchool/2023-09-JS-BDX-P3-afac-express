9d7759c4a9d5a93ca859c6b958e99a024f57c1cc
Florian BRUN <flo33.brun@hotmail.fr>
2023-12-15 09:46:06 +0100

    Added modifications Footer.jsx and Footer.scss

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,43 +1,60 @@
 import React from "react";
 import { MDBFooter, MDBContainer, MDBCol, MDBRow } from "mdb-react-ui-kit";
+import logoTelephone from "../../assets/footer/logoTelephone.svg";
+import logoMail from "../../assets/footer/logoMail.svg";
+import logoObjetTemoin from "../../assets/footer/logoObjetTemoin.svg";
+import logoDepartementReunion from "../../assets/footer/logoDepartementReunion.svg";
+import "./Footer.scss";
 
-function Footer() {
-  <MDBFooter bgColor="light" className="text-center text-lg-left">
-    <MDBContainer className="p-4">
-      <MDBRow>
-        <MDBCol lg="6" md="12" className="mb-4 mb-md-0">
-          <h5 className="text-uppercase">Footer text</h5>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste atque
-            ea quis molestias. Fugiat pariatur maxime quis culpa corporis vitae
-            repudiandae aliquam voluptatem veniam, est atque cumque eum delectus
-            sint!
-          </p>
-        </MDBCol>
+export default function Footer() {
+  return (
+    <MDBFooter className="text-center text-lg-left container-footer">
+      <MDBContainer className="p-4">
+        <MDBRow>
+          <MDBCol lg="6" md="12" className="mb-4 mb-md-0">
+            <div className="contact">
+              <h2 className="title-contact">Nous Contacter</h2>
+              <div className="container number">
+                <img
+                  className="logo-telephone"
+                  src={logoTelephone}
+                  alt="icone telephone"
+                />
+                <p className="telephone-number">+262 692 31 83 98</p>
+              </div>
+              <div className="container mail">
+                <img className="logo-mail" src={logoMail} alt="icone mail" />
+                <p className="adress-mail">afac974@gmail.com</p>
+              </div>
+            </div>
+          </MDBCol>
 
-        <MDBCol lg="6" md="12" className="mb-4 mb-md-0">
-          <h5 className="text-uppercase">Footer text</h5>
+          <MDBCol lg="6" md="12" className="mb-4 mb-md-0">
+            <div className="partnerships">
+              <h2 className="title-partnerships">Partenaires</h2>
+              <div className="container-partnerships">
+                <img
+                  className="logo-left"
+                  src={logoObjetTemoin}
+                  alt="objet-temoin-logo"
+                />
+                <img
+                  className="logo-right"
+                  src={logoDepartementReunion}
+                  alt="reunion-department-logo"
+                />
+              </div>
+            </div>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
 
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste atque
-            ea quis molestias. Fugiat pariatur maxime quis culpa corporis vitae
-            repudiandae aliquam voluptatem veniam, est atque cumque eum delectus
-            sint!
-          </p>
-        </MDBCol>
-      </MDBRow>
-    </MDBContainer>
-
-    <div
-      className="text-center p-3"
-      style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
-    >
-      &copy; {new Date().getFullYear()} Copyright:{" "}
-      <a className="text-dark" href="https://mdbootstrap.com/">
-        MDBootstrap.com
-      </a>
-    </div>
-  </MDBFooter>;
+      <div
+        className="text-center p-3 copyright"
+        style={{ backgroundColor: "#7b273d" }}
+      >
+        &copy; {new Date().getFullYear()} Copyright: Afac express
+      </div>
+    </MDBFooter>
+  );
 }
-
-export default Footer;
