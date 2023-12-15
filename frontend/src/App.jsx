b9c4971c60b77d3fa20e55fa473fc054552e4c1b
Nelia <137152184+NeliaCou@users.noreply.github.com>
2023-12-15 10:04:38 +0100

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,19 @@
-import "./App.scss";
+import { MDBContainer } from "mdb-react-ui-kit";
 import { Outlet } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
 import "./components/Footer/Footer.scss";
+import "./style/Footer.scss";
 import Navigation from "./components/Navigation";
 
 function App() {
   return (
-    <div>
+    <>
       <Navigation />
-      <Outlet />
+      <MDBContainer className="d-flex flex-column">
+        <Outlet />
+      </MDBContainer>
       <Footer />
-    </div>
+    </>
   );
 }
 
