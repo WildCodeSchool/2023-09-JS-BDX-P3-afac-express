a30c6e39dee481f402fa912b79875523ee48b28e
Nelia <nelia.courtais@live.fr>
2023-12-16 17:37:07 +0100

    add back debut
    Co-authored-by: BRUNFlorian <BRUNFlorian@users.noreply.github.com>

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,23 +1,25 @@
-const express = require("express");
+// const express = require("express");
 
-const router = express.Router();
+// const router = express.Router();
 
-/* ************************************************************************* */
-// Define Your API Routes Here
-/* ************************************************************************* */
+// const userControllers = require("./controllers/userControllers");
 
-// Import itemControllers module for handling item-related operations
-const itemControllers = require("./controllers/itemControllers");
+// router.get("/users", userControllers.getUsers);
 
-// Route to get a list of items
-router.get("/items", itemControllers.browse);
+// router.get("/users/:id", userControllers)
 
-// Route to get a specific item by ID
-router.get("/items/:id", itemControllers.read);
+// // Import itemControllers module for handling item-related operations
+// const itemControllers = require("./controllers/itemControllers");
 
-// Route to add a new item
-router.post("/items", itemControllers.add);
+// // Route to get a list of items
+// router.get("/items", itemControllers.browse);
 
-/* ************************************************************************* */
+// // Route to get a specific item by ID
+// router.get("/items/:id", itemControllers.read);
 
-module.exports = router;
+// // Route to add a new item
+// router.post("/items", itemControllers.add);
+
+// /* ************************************************************************* */
+
+// module.exports = router;
