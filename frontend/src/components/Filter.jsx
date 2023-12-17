481a4ea48c83761be283362dbddb76c2e4c7d201
Nelia <nelia.courtais@live.fr>
2023-12-17 10:20:11 +0100

    add artist filter

--- frontend/src/components/Filter.jsx
+++ frontend/src/components/Filter.jsx
@@ -4,30 +4,28 @@ import {
   MDBDropdownMenu,
   MDBDropdownToggle,
 } from "mdb-react-ui-kit";
+import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 
-const artists = [
-  { id: 1, name: "Lara Sousa" },
-  { id: 2, name: "Deepa Bauhadoor" },
-  { id: 3, name: "Mahefa Dimbiniaina Randrianarivelo" },
-  { id: 4, name: "Mathilde Neri" },
-];
-
-function Filter({ match }) {
-  const artistId = match.params.id;
+function Filter({ artists, onSelectArtist }) {
   return (
-    <MDBDropdown className="d-flex justify-content-center pb-5">
+    <MDBDropdown className="d-flex justify-content-center pb-4 pt-3">
       <MDBDropdownToggle
         tag="a"
         className="btn btn-primary bg-transparent text-dark"
       >
-        Autres artistes {artistId}
+        Autres artistes
       </MDBDropdownToggle>
 
       <MDBDropdownMenu>
         {artists.map((artist) => (
-          <MDBDropdownItem key={artist.id} link href={`/artist/${artist.id}`}>
-            {artist.name}
+          <MDBDropdownItem key={artist.id}>
+            <Link
+              to={`/artist/${artist.id}`}
+              onClick={() => onSelectArtist(artist)}
+            >
+              {artist.name}
+            </Link>
           </MDBDropdownItem>
         ))}
       </MDBDropdownMenu>
@@ -36,10 +34,13 @@ function Filter({ match }) {
 }
 
 Filter.propTypes = {
-  match: PropTypes.shape({
-    params: PropTypes.shape({
-      id: PropTypes.string.isRequired,
-    }).isRequired,
-  }).isRequired,
+  artists: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+  onSelectArtist: PropTypes.func.isRequired,
 };
+
 export default Filter;
