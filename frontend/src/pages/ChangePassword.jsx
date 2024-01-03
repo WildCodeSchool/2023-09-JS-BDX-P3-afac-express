2c80df6313f61b08374d6bfdd597945e6b3bcae9
Florian BRUN <flo33.brun@hotmail.fr>
2024-01-03 14:35:53 +0100

    Added page ChangePassword.jsx

--- frontend/src/pages/ChangePassword.jsx
+++ frontend/src/pages/ChangePassword.jsx
@@ -1,56 +1,17 @@
 import React from "react";
-import { MDBInput, MDBCol, MDBRow, MDBBtn } from "mdb-react-ui-kit";
+import { MDBInput, MDBBtn } from "mdb-react-ui-kit";
 
 export default function ChangePassword() {
   return (
     <form className="user-form">
-      <h1>Changer le mot de passe</h1>
-      <h3>Créateur</h3>
-      <MDBInput
-        className="mb-4"
-        type="email"
-        id="form2Example1"
-        label="Artiste"
-      />
-      <MDBInput
-        className="mb-4"
-        type="password"
-        id="form2Example2"
-        label="Biographie"
-      />
+      <h1>Changement du mot de passe</h1>
+      <h3>Nouveau mot de passe</h3>
+      <MDBInput className="mb-4" type="password" id="form2Example1" />
+      <h3>Confirmez le nouveau mot de passe</h3>
+      <MDBInput className="mb-4" type="password" id="form2Example2" />
+
       <MDBBtn type="submit" className="mb-4" block>
-        Ajouter une image
-      </MDBBtn>
-      <MDBBtn type="submit" className="mb-4" block>
-        Valider
-      </MDBBtn>
-      <h3>Oeuvres</h3>
-      <MDBInput className="mb-4" type="artName" id="artName" label="Titre" />
-      <MDBInput
-        className="mb-4"
-        type="artistArt"
-        id="artistArt"
-        label="Si existant choix de l'artiste"
-      />
-      <MDBRow className="mb-4">
-        <MDBCol>
-          <MDBInput id="height" label="Largeur" />
-        </MDBCol>
-        <MDBCol>
-          <MDBInput id="width" label="Longueur" />
-        </MDBCol>
-      </MDBRow>
-      <MDBInput
-        className="mb-4"
-        type="password"
-        id="createLocation"
-        label="Lieux de création"
-      />
-      <MDBBtn type="submit" className="mb-4" block>
-        Ajouter une image
-      </MDBBtn>
-      <MDBBtn type="submit" className="mb-4" block>
-        Valider
+        Réinitialiser mon mot de passe
       </MDBBtn>
     </form>
   );
