a0b13933628fc2cc83532adc3eb3f29182e28565
BRUNFlorian <121351108+BRUNFlorian@users.noreply.github.com>
2024-01-08 09:49:42 +0100

    Update schema.sql

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -9,8 +9,8 @@ DROP TABLE IF EXISTS artist;
 CREATE TABLE
     artist (
         id int primary key NOT NULL AUTO_INCREMENT,
-        name varchar(255) ,
-        description varchar(10000)
+        name varchar(255) NOT NULL,
+        description varchar(10000) NOT NULL
     )
 
 INSERT INTO
