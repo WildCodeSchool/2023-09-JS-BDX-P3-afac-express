94e8a9ee73be6c901748cc02045cb8adc65e127b
Florian BRUN <flo33.brun@hotmail.fr>
2024-01-23 10:05:11 +0100

    Addition of a new NewGallery.jsx component, which will replace the Gallery component after adjustment

---
+++ frontend/src/pages/NewGallery.jsx
@@ -0,0 +1,83 @@
+import {
+  MDBAnimation,
+  MDBBtn,
+  MDBCol,
+  MDBContainer,
+  MDBRow,
+} from "mdb-react-ui-kit";
+import { Link } from "react-router-dom";
+
+import { useApp } from "../context/AppContext";
+
+function Gallery() {
+  const { artCollection } = useApp();
+
+  return (
+    <MDBContainer fluid className="pt-5">
+      <h2
+        className="fs-1 text text-center fw-bold pt-5 mb-6"
+        style={{ color: "#7b273d" }}
+      >
+        Titre à définir
+      </h2>
+      <MDBRow>
+        {artCollection.map((artwork, index) => (
+          <MDBCol
+            lg="4"
+            md="12"
+            className="mb-4"
+            key={artwork.id}
+            itemID={index + 1}
+          >
+            <MDBAnimation
+              reset
+              tag="img"
+              repeatOnScroll
+              start="onScroll"
+              animation="fade-in"
+              duration={500}
+              src={artwork.image}
+              className="img-fluid shadow-1-strong rounded"
+            />
+            <h3
+              className="fs-4 fw-bold text-center text-truncate mt-3"
+              style={{ color: "#7b273d" }}
+            >
+              {artwork.title}
+            </h3>
+            <h3 className=" fst-italic fs-5 text-center mt-3 pb-3">
+              {artwork.artist_name}
+            </h3>
+
+            <div className="d-flex justify-content-center">
+              <MDBBtn tag="span" className="m-1 text-white">
+                <Link
+                  to={{
+                    pathname: `/artworks/${artwork.id}`,
+                    state: {
+                      artwork: {
+                        id: artwork.id,
+                        image: artwork.image,
+                        title: artwork.title,
+                        artistName: artwork.artist_name,
+                        dimension: artwork.dimension,
+                        creationPlace: artwork.creation_place,
+                      },
+                    },
+                  }}
+                  className="text-white"
+                >
+                  Plus d'infos
+                </Link>
+              </MDBBtn>
+              <MDBBtn tag="span" className="m-1 text-white">
+                Ajouter aux favoris
+              </MDBBtn>
+            </div>
+          </MDBCol>
+        ))}
+      </MDBRow>
+    </MDBContainer>
+  );
+}
+export default Gallery;
