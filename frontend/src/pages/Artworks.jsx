26ac56e9fa3a958a2cdfb3a602b31ffad61f694f
Nelia <nelia.courtais@live.fr>
2024-01-23 15:21:37 +0100

    change artwork page & fix like button

--- frontend/src/pages/Artworks.jsx
+++ frontend/src/pages/Artworks.jsx
@@ -1,17 +1,13 @@
-import {
-  MDBCard,
-  MDBCardBody,
-  MDBCardText,
-  MDBContainer,
-  MDBCarousel,
-  MDBCarouselItem,
-  MDBCardTitle,
-} from "mdb-react-ui-kit";
-import { useApp } from "../context/AppContext";
-import FilterArtworks from "../components/Filter/FilterArtworks";
+import { MDBContainer } from "mdb-react-ui-kit";
+// import { Swiper, SwiperSlide } from "swiper/swiper-react";
+// import { useState } from "react";
+import Art from "../components/Art";
 
 function Artworks() {
-  const { artCollection } = useApp();
+  // const [art, setArt] = useState();
+  // const setInformation = (e) => {
+  //   setArt(e.target.value);
+  // };
 
   return (
     <MDBContainer fluid className="pt-5 pb-5">
@@ -21,42 +17,16 @@ function Artworks() {
       >
         Patrimoine iconographique de l'océan indien
       </h3>
-
-      <FilterArtworks />
-
-      {artCollection?.length && (
-        <MDBCarousel showControls interval={10000}>
-          {artCollection.map((art, index) => (
-            <MDBCarouselItem key={art.id} id={index + 1}>
-              <img src={art.image} className="d-block w-100" alt={art.title} />
-              <MDBCard>
-                <MDBCardBody>
-                  <MDBCardTitle
-                    className="fst-italic text-center fs-5"
-                    style={{ color: "#7b273d" }}
-                  >
-                    {art.title}
-                  </MDBCardTitle>
-                  <MDBCardText className="text-center lh-sm">
-                    <h3 className="fs-3 text text-center pt-2 fw-bold pb-2">
-                      {art.artist_name}
-                    </h3>
-                    {art.dimension && (
-                      <span>
-                        {art.dimension}
-                        <br />
-                      </span>
-                    )}
-                    {art.creation_place && <span>{art.creation_place}</span>}
-                  </MDBCardText>
-                </MDBCardBody>
-              </MDBCard>
-            </MDBCarouselItem>
-          ))}
-        </MDBCarousel>
-      )}
-
-      {!artCollection?.length && <p>Les données ne sont pas disponibles.</p>}
+      {/* <Swiper
+        spaceBetween={50}
+        slidesPerView={1}
+        onSlideChange={() => console.info("slide change")}
+        onSwiper={(swiper) => console.info(swiper)}
+      >
+        <SwiperSlide> */}
+      <Art />
+      {/* </SwiperSlide>
+      </Swiper> */}
     </MDBContainer>
   );
 }
