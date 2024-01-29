3d7d8cb6e53d8d5bebb067cd73e2ed49e13ded3a
Benoit-Hayet <hayetbenoit@gmail.com>
2024-01-29 17:29:27 +0100

    Selector Ok

--- frontend/src/components/Filter/FilterArtistAdmin.jsx
+++ frontend/src/components/Filter/FilterArtistAdmin.jsx
@@ -1,19 +1,18 @@
 import React from "react";
-import { MDBSelect } from "mdb-react-ui-kit";
+import { useApp } from "../../context/AppContext";
 
 export default function FilterArtistAdmin() {
+  const { artistCollection } = useApp();
+
   return (
-    <MDBSelect
-      data={[
-        { text: "One", value: 1 },
-        { text: "Two", value: 2 },
-        { text: "Three", value: 3 },
-        { text: "Four", value: 4 },
-        { text: "Five", value: 5 },
-        { text: "Six", value: 6 },
-        { text: "Seven", value: 7 },
-        { text: "Eight", value: 8 },
-      ]}
-    />
+    <div>
+      <select>
+        {artistCollection.map((artist) => (
+          <option key={artist.id} value={artist.id}>
+            {artist.name}
+          </option>
+        ))}
+      </select>
+    </div>
   );
 }
